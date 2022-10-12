5e6b5030719ca32e9a59185f0cb1eb054e745574
JHTran <tr.h.jonathan@gmail.com>
2022-10-11 20:47:18 -0400

    path variable annotation

--- src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28min/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28min.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 // REST API
@@ -18,6 +19,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         
         return new HelloWorldBean("Hello World");
+    }
+    
+    @GetMapping("/hello-world/path-var/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
     
 }
